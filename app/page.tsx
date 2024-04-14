fbf826c311ea2defc1986a1aafe185b1cf196d91
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-04-14 19:23:46 -0300

    update

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-black">
+        Municípios do Brasil
+      </h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{tsx,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+}
